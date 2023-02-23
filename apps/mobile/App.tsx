f17b11cb52bc48193c1c11fea51451c5268965f6
Drew Harris <harrisd@smu.edu>
2023-02-22 20:08:18 -0600

    Fixed expo

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View >
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
-
