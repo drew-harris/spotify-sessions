dd0295fadbc44b73399ef60be88dc00e64bbd7b1
Drew Harris <harrisd@smu.edu>
2023-03-11 16:58:09 -0600

    Remove dead trpc query

--- apps/next/src/pages/index.tsx
+++ apps/next/src/pages/index.tsx
@@ -1,6 +1,3 @@
-import { trpc } from "@/utils/trpc";
-
 export default function Home() {
-  const { data } = trpc.hello.useQuery({ text: "Drew" });
-  return <div>{data ? data.message : null}</div>;
+  return <div>Hello</div>;
 }
